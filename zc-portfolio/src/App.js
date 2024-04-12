3524e89d7c83a14a58d40cd48a3ad33a5436d235
poseizen <zzc120@ic.ac.uk>
2024-04-12 21:53:06 +0100

    added portfolio link to page

--- zc-portfolio/src/App.js
+++ zc-portfolio/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import OttertunityPage from './components/OttertunityPage/OttertunityPage';
+import HiSayang from './components/HiSayang/HiSayang';
+import MNightPage from './components/MNightPage/MNightPage';
+import LobbyPage from './components/LobbyPage/LobbyPage';
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} />  */}
+      <Route path="/lobby" element={<LobbyPage />} /> 
       <Route path="/ottertunity" element={<OttertunityPage />} />
+      <Route path="mnight" element={<MNightPage />} />
+      <Route path="/hisayang" element={<HiSayang />} />
     </Routes>
   );
 }
